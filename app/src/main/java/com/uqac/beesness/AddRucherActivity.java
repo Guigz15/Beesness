0cf81e3214667a5e0d5a8e0769608d81326ee15e
arnold33 <tchuisseuarnold52@gmail.com>
2022-11-02 12:07:57 -0400

    Feat: Add Rucher page ui done

---
+++ app/src/main/java/com/uqac/beesness/AddRucherActivity.java
@@ -0,0 +1,14 @@
+package com.uqac.beesness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRucherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_rucher);
+    }
+}
